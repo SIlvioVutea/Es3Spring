9f0268d197d6d0b792e60bf61b9c8ae78b0fc587
SIlvioVutea <silviovutea@gmail.com>
2024-05-26 11:42:17 +0200

    exercise done

---
+++ src/main/java/com/example/Es3Spring/Es3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Es3Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es3SpringApplication.class, args);
+	}
+
+}
